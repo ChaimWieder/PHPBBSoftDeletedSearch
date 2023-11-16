643efcdd770ac2c952a24a3bd6f1819439bece7a
ChaimWieder <113867156+ChaimWieder@users.noreply.github.com>
2023-11-16 18:57:13 -0500

    Update common.php

--- softdeletedsearch/language/yi/common.php
+++ softdeletedsearch/language/yi/common.php
@@ -6,7 +6,7 @@ if (!defined('IN_PHPBB'))
 
 $lang = array_merge($lang, array(
     'SHOW_DELETED'   => 'ווייז געמעקטע',
-    'SEARCH_USER_DELETED_POSTS' => 'זוך באנוצערס געמעקטע תגובות',
-    
-    
+    'SEARCH_USER_DELETED_POSTS' => 'זוך',
+    'USER_POSTS_DELETED'=> 'באנוצערס געמעקטע תגובות:',
+
 ));
