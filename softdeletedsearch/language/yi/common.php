7b5e1d6f423bb0880a3fbe6bf0a9eba982775e8d
ChaimWieder <113867156+ChaimWieder@users.noreply.github.com>
2023-11-16 18:23:57 -0500

    Create yi/common.php

---
+++ softdeletedsearch/language/yi/common.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('IN_PHPBB'))
+{
+    exit;
+}
+
+$lang = array_merge($lang, array(
+    'SHOW_DELETED'   => 'ווייז געמעקטע',
+    'SEARCH_USER_DELETED_POSTS' => 'זוך באנוצערס געמעקטע תגובות',
+    
+    
+));
